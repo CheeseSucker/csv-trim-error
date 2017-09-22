de98783a385ce6cf1d87f229a255bfb7ecf58ceb
CheeseSucker <cheesesucker@gmail.com>
2017-09-22 10:49:14 +0200

    Initial commit

---
+++ CsvBug/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace CsvBug
+{
+    class Program
+    {
+        const string CsvOk = "Name\r\nA B\r\n";
+        const string CsvOk2 = "Name\r\nA-B";
+        const string CsvNotOk = "Name\r\nA B";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ok");
+            ReadCsv(CsvOk);
+
+            Console.WriteLine("Ok");
+            ReadCsv(CsvOk2);
+
+            Console.WriteLine("Will fail");
+            ReadCsv(CsvNotOk);
+            Console.WriteLine("Is it fixed?");
+        }
+
+        private static void ReadCsv(string content)
+        {
+            var csvConfig = new Configuration
+            {
+                TrimOptions = TrimOptions.Trim
+            };
+
+            var records = new List<Row>();
+            using (var fileStream = new StringReader(content))
+            {
+                using (var reader = new CsvReader(fileStream, csvConfig))
+                {
+                    records.AddRange(reader.GetRecords<Row>());
+                }
+            }
+        }
+
+        public class Row
+        {
+            public string Name { get; set; }
+        }
+    }
+
+
+}
